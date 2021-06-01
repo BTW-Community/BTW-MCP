fcf0b27cfe236b3a478c1889c622a6b761bea9eb
yany2 <46777856+yany2@users.noreply.github.com>
2021-06-01 19:38:08 +0200

    rename jars directly when downloading

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import shutil
 import subprocess
 import distutils.dir_util
 
-
 def download():
     links = [
-    "https://launcher.mojang.com/v1/objects/f9ae3f651319151ce99a0bfad6b34fa16eb6775f/server.jar",
-    "https://launcher.mojang.com/v1/objects/465378c9dc2f779ae1d6e8046ebc46fb53a57968/client.jar",
-    "https://libraries.minecraft.net/net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar",
-    "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl/2.9.1/lwjgl-2.9.1.jar",
-    "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl_util/2.9.1/lwjgl_util-2.9.1.jar",
-    "https://libraries.minecraft.net/net/java/jinput/jinput-platform/2.0.5/jinput-platform-2.0.5-natives-windows.jar",
-    "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.1/lwjgl-platform-2.9.1-natives-windows.jar"
+        ("minecraft_server.jar", "https://launcher.mojang.com/v1/objects/f9ae3f651319151ce99a0bfad6b34fa16eb6775f/server.jar"),
+        ("minecraft.jar", "https://launcher.mojang.com/v1/objects/465378c9dc2f779ae1d6e8046ebc46fb53a57968/client.jar"),
+        ("jinput.jar", "https://libraries.minecraft.net/net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar"),
+        ("lwjgl.jar", "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl/2.9.1/lwjgl-2.9.1.jar"),
+        ("lwjgl_util.jar", "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl_util/2.9.1/lwjgl_util-2.9.1.jar"),
+        ("jinput.zip", "https://libraries.minecraft.net/net/java/jinput/jinput-platform/2.0.5/jinput-platform-2.0.5-natives-windows.jar"),
+        ("lwjgl.zip", "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.1/lwjgl-platform-2.9.1-natives-windows.jar")
     ]
     os.mkdir("downloads")
     os.chdir("downloads")
     for link in links:
         print(link)
-        subprocess.run(["curl", link, "--output", link.split("/")[-1] ])
+        subprocess.run(["curl", link[1], "--output", link[0] ])
     os.chdir("..")
 
 # setup mcp jars folder
@@ -30,20 +29,20 @@ def mcp_jars():
     shutil.unpack_archive("mcp751.zip")
     os.remove("mcp751.zip")
     os.chdir("jars")
-    shutil.copyfile("../../downloads/server.jar", "minecraft_server.jar")
+    shutil.copyfile("../../downloads/minecraft_server.jar", "minecraft_server.jar")
     shutil.copytree("../../files/resources", "resources")
     os.mkdir("bin")
     os.chdir("bin")
-    shutil.copyfile("../../../downloads/client.jar", "minecraft.jar")
-    shutil.copyfile("../../../downloads/jinput-2.0.5.jar", "jinput.jar")
-    shutil.copyfile("../../../downloads/lwjgl-2.9.1.jar", "lwjgl.jar")
-    shutil.copyfile("../../../downloads/lwjgl_util-2.9.1.jar", "lwjgl_util.jar")
+    shutil.copyfile("../../../downloads/minecraft.jar", "minecraft.jar")
+    shutil.copyfile("../../../downloads/jinput.jar", "jinput.jar")
+    shutil.copyfile("../../../downloads/lwjgl.jar", "lwjgl.jar")
+    shutil.copyfile("../../../downloads/lwjgl_util.jar", "lwjgl_util.jar")
     os.mkdir("natives")
     os.chdir("natives")
-    shutil.copyfile("../../../../downloads/jinput-platform-2.0.5-natives-windows.jar", "jinput.zip")
+    shutil.copyfile("../../../../downloads/jinput.zip", "jinput.zip")
     shutil.unpack_archive("jinput.zip")
     os.remove("jinput.zip")
-    shutil.copyfile("../../../../downloads/lwjgl-platform-2.9.1-natives-windows.jar", "lwjgl.zip")
+    shutil.copyfile("../../../../downloads/lwjgl.zip", "lwjgl.zip")
     shutil.unpack_archive("lwjgl.zip")
     os.remove("lwjgl.zip")
     os.chdir("../../../..")
@@ -66,6 +65,7 @@ def recompile():
     os.chdir("mcp")
     subprocess.run(["runtime/bin/python/python_mcp", "runtime/recompile.py"])
     os.chdir("..")
+
 
 
 download()
